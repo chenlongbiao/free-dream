0b484e7a0099a9acec765f41399beec72dbefe43
chen <714147655@qq.com>
2017-07-10 23:49:06 +0800

    new file:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   src/main/java/com/freedream/FreeDreamApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/freedream/FreeDreamApplicationTests.java

---
+++ src/main/java/com/freedream/FreeDreamApplication.java
@@ -0,0 +1,12 @@
+package com.freedream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeDreamApplication.class, args);
+	}
+}
