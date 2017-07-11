a2f60bd5f175eaa15cf1e1176093cc1d3f1983ad
陈龙飚 <714147655@qq.com>
2017-07-11 18:09:32 +0800

    整合jersey

--- src/main/java/com/freedream/FreeDreamApplication.java
+++ src/main/java/com/freedream/FreeDreamApplication.java
@@ -2,10 +2,12 @@ package com.freedream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan
 public class FreeDreamApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FreeDreamApplication.class, args);
 	}
